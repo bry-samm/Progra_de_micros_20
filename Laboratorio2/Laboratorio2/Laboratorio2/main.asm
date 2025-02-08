9a26b0d73d9448b0e9e0855c65375cd354f8e0e2
Bryan Morales <mor23283@uvg.edu.gt>
2025-02-07 21:28:27 -0600

    Update main.asm

--- Laboratorio2/Laboratorio2/Laboratorio2/main.asm
+++ Laboratorio2/Laboratorio2/Laboratorio2/main.asm
@@ -8,7 +8,7 @@
 .include "M328PDEF.inc"
 .cseg
 .org 0x0000
-
+.def COUNTER = R20
 /******************************************************************/
 //Configuración de la pila 
 /*****************************************************************/
@@ -19,3 +19,40 @@
 //=================================================================================
 //Configurar el microcontrolador (MCU)
 SETUP:
+	//Prescaler del oscilador
+	LDI		R16, (1 << CLKPCE)    ; Habilita la escritura en CLKPR
+	STS		CLKPR, R16
+	LDI		R16, (1 << CLKPS2)      ; Configura prescaler a 16 (16 MHz / 16 = 1 MHz)
+	STS		CLKPR, R16
+	// Inicializar timer0
+	CALL INIT_TMR0
+	//PORTC como salida e inicialmente apagado 
+	LDI		R16, 0xFF
+	OUT		DDRC, R16	//Setear puerto C como salida
+	LDI		R16, 0x00
+	OUT		PORTC, R16	//Apagar puerto C
+
+	LDI		R21, 0x00
+	// Loop Infinito
+MAIN_LOOP:
+	IN		R16, TIFR0 // Leer registro de interrupci n de TIMER 0 
+	SBRS	R16, TOV0 // Salta si el bit 0 est "set" (TOV0 bit) 
+	RJMP	MAIN_LOOP // Reiniciar loop
+	SBI		TIFR0, TOV0 // Limpiar bandera de "overflow"
+	LDI		R16, 100
+	OUT		TCNT0, R16 // Volver a cargar valor inicial en TCNT0
+	INC		COUNTER
+	CPI		COUNTER, 10 // R20 = 50 after 500ms (since TCNT0 is set to 10 ms)
+	BRNE	MAIN_LOOP
+	CLR		COUNTER
+	INC		R21
+	OUT		PORTC, R21
+	RJMP	MAIN_LOOP
+/****************************************/
+// NON-Interrupt subroutines
+INIT_TMR0:
+	LDI		R16, (1<<CS01) | (1<<CS00)
+	OUT		TCCR0B, R16 // Setear prescaler del TIMER 0 a 64
+	LDI		R16, 100
+	OUT		TCNT0, R16 // Cargar valor inicial en TCNT0
+	RET
