90d7be7a533e16c73afc46f2e8d574c2364868ab
Bryan Morales <mor23283@uvg.edu.gt>
2025-04-02 20:55:41 -0600

    Lógica de alarma
    Se implementó la lógica para comparación de contadores

--- Laboratorio4/Laboratorio4/Laboratorio4/main.c
+++ Laboratorio4/Laboratorio4/Laboratorio4/main.c
@@ -3,13 +3,33 @@
  * 
  * Created: 28/03/2025 23:09:24
  * Author: Bryan Morales
- * Description:
+ * Description: Contador binario de 8 bits controlado por botones paralelo a un contador hexadecimal de 8 bits controlado por
+ * un potenciómetro el cual se muestra en un display, si el contador hexadecimal es mayor al binario se enciente una led como alarma 
  */
 //************************************************************************************
-// Encabezado (Libraries)#define F_CPU 16000000#include <avr/io.h>#include <avr/interrupt.h>uint8_t		contador_mostrar_dis;uint8_t		contador_leds;uint8_t		lectura_ADC;
+// Encabezado (Libraries)#define F_CPU 16000000#include <avr/io.h>#include <avr/interrupt.h>#include <util/delay.h>uint8_t		contador_mostrar_dis;uint8_t		contador_leds;uint8_t		lectura_ADC;
 uint8_t		segmentos_unidades;
-uint8_t		segmentos_decenas;int		valores_display[16] = {0x7D, 0x48, 0x3E, 0x6E, 0x4B, 0x67, 0x77, 0x4C, 0x7F, 0x6F, 0x5F, 0x73, 0x35, 0x7A, 0x7A, 0x17 };/****************************************/// Function prototypesvoid setup();void RESTA_BOTON(void);
-void SUMA_BOTON(void);void initTMR0();void initADC();/****************************************/// Main Functionint main(void){	setup();		while (1)	{			}}/****************************************/// NON-Interrupt subroutinesvoid setup(){
+uint8_t		segmentos_decenas;int		valores_display[16] = {0x7D, 0x48, 0x3E, 0x6E, 0x4B, 0x67, 0x77, 0x4C, 0x7F, 0x6F, 0x5F, 0x73, 0x35, 0x7A, 0x37, 0x17 };/****************************************/// Function prototypesvoid setup();void RESTA_BOTON(void);
+void SUMA_BOTON(void);void initTMR0();void initADC();/****************************************/// Main Functionint main(void)
+{
+	setup();  // Configurar ADC y otros periféricos
+	
+	while (1)
+	{
+		//Alternativa si se desea colocar la lógica en en main
+		//Se debe de agregar un delay, prefiero dejar la comparación en la interrupción para que sea instantáneo
+		/* 
+			if (lectura_ADC >= contador_leds) {
+				PORTC |= (1 << PORTC4);  // Apagar LED si ADC es mayor o igual
+			}
+			else {
+				PORTC &= ~(1 << PORTC4);   // Encender LED si ADC es menor
+				}
+				_delay_ms(10);  // Pequeño retraso para evitar interferencias
+				*/
+	}
+}
+/****************************************/// NON-Interrupt subroutinesvoid setup(){
 	cli();
 	
 	CLKPR	= (1 << CLKPCE); //Habilitar la configuración del oscilador CPU
@@ -19,17 +39,17 @@ void SUMA_BOTON(void);void initTMR0();void initADC();/**********************
 	PORTD = 0x00;    // PORTD inicialmente apagado (LEDS)
 	
 	// Configuración de botones en PORTB
-	DDRB &= ~((1 << PB0) | (1 << PB1));  // PB0 y PB1 como entradas
-	PORTB |= (1 << PB0) | (1 << PB1);    // Activar pull-ups 
+	DDRB &= ~((1 << PORTB0) | (1 << PORTB1));  // PB0 y PB1 como entradas
+	PORTB |= (1 << PORTB0) | (1 << PORTB1);    // Activar pull-ups 
 	
-	// Configuración de transistores en PORTC para multiplexar
-	DDRC |= (1 << PC0) | (1 << PC1) | (1 << PC2);  // PORTC como salida
+	// Configuración de transistores y led en PORTC para multiplexar y mostrar alarma
+	DDRC |= (1 << PORTC0) | (1 << PORTC1) | (1 << PORTC2) | (1 << PORTC4);  // PORTC como salida
 	
-	PORTC &= ~((1 << PC0) | (1 << PC1) | (1 << PC2));  // Transistores PC0, PC1, PC2 apagados inicialmente
+	PORTC &= ~((1 << PORTC0) | (1 << PORTC1) | (1 << PORTC2) | (1 << PORTC4));  // Transistores t led inicialmente apagado
 	
 	// Habilitar interrupción en PB0 y PB1 (PCI0)
 	PCICR |= (1 << PCIE0);               // Habilitar interrupciones en PORTB
-	PCMSK0 |= (1 << PCINT0) | (1 << PCINT1);  // PB0 y PB1 (PCINT0 y PCINT1)
+	PCMSK0 |= (1 << PCINT0) | (1 << PCINT1);  // PB0 y PB1 
 	
 	initTMR0();
 	initADC();
@@ -53,10 +73,10 @@ void SUMA_BOTON(void);void initTMR0();void initADC();/**********************
 	ADCSRA	|= (1<< ADSC);
 }/****************************************/// Interrupt routinesISR(PCINT0_vect) {
 	// Verificar estado actual de los botones
-	if (!(PINB & (1 << PB0))) {  // Si PB0 está presionado (pull-up)
+	if (!(PINB & (1 << PORTB0))) {  // Si PB0 está presionado (pull-up)
 		RESTA_BOTON();
 	}
-	if (!(PINB & (1 << PB1))) {  // Si PB1 está presionado
+	if (!(PINB & (1 << PORTB1))) {  // Si PB1 está presionado
 		SUMA_BOTON();
 	}
 }
@@ -65,23 +85,30 @@ ISR(TIMER0_OVF_vect){
 	TCNT0 = 200;
 	contador_mostrar_dis++;
 	PORTD = 0;  // Apagar todos los segmentos primero
-	PORTC &= ~((1 << PC0) | (1 << PC1) | (1 << PC2));  // Apagar todos los transistores
+	PORTC &= ~((1 << PORTC0) | (1 << PORTC1) | (1 << PORTC2));  // Apagar todos los transistores
 	
 	switch(contador_mostrar_dis){
 		case 1:
-		PORTC |= (1 << PC2);  // Encender transistor display 1
-		PORTD = contador_leds;  // Mostrar valor para display 1
+		PORTC |= (1 << PORTC2);  // Encender transistor de leds 
+		PORTD = contador_leds;  // Mostrar contador de leds
 		break;
 		case 2:
-		PORTC |= (1 << PC0);  // Encender transistor display 2
-		PORTD = segmentos_unidades;  // Mostrar valor para display 2
+		PORTC |= (1 << PORTC0);  // Encender transistor display 1
+		PORTD = segmentos_decenas;  // Mostrar valor para display 1
 		break;
 		case 3:
-		PORTC |= (1 << PC1);  // Encender transistor display 3
-		PORTD = segmentos_decenas;  // Mostrar valor para display 3
+		PORTC |= (1 << PORTC1);  // Encender transistor display 2
+		PORTD = segmentos_unidades;  // Mostrar valor para display 2
 		break;
 		default:
 		contador_mostrar_dis = 0;
+	}
+	
+	if (lectura_ADC >= contador_leds) {
+		PORTC |= (1 << PORTC4);  // Apagar LED si ADC es mayor o igual
+	}
+	else {
+		PORTC &= ~(1 << PORTC4);   // Encender LED si ADC es menor
 	}
 }
 
@@ -89,16 +116,8 @@ ISR(ADC_vect)
 {
 	lectura_ADC = ADCH;
 	
-	uint8_t indice = (lectura_ADC * 16) / 256;  // Convertir ADC a índice 
+	segmentos_decenas = valores_display[lectura_ADC >> 4];  // Dígito alto 
+	segmentos_unidades = valores_display[lectura_ADC & 0x0F];  // Dígito bajo
 	
-	uint8_t numero_a_mostrar = valores_display[indice];  // Obtener valor del array
-	uint8_t unidades = numero_a_mostrar % 10;
-	uint8_t decenas  = (numero_a_mostrar / 10) % 10;
-
-	// Obtener los dígitos de 7 segmentos
-	segmentos_unidades = valores_display[unidades];
-	segmentos_decenas  = valores_display[decenas];
-	
-	
-	ADCSRA	|= (1<< ADSC);
+	ADCSRA	|= (1<< ADSC);		//Inicia una nueva lectura
 }
