76eb1161ff9f9935ece56245c7bd5b381648a116
Bryan Morales <mor23283@uvg.edu.gt>
2025-04-06 21:51:15 -0600

    Update PWM_SERVO.c

--- Laboratorio5/Laboratorio5/Laboratorio5/PWM_SERVO/PWM_SERVO.c
+++ Laboratorio5/Laboratorio5/Laboratorio5/PWM_SERVO/PWM_SERVO.c
@@ -10,7 +10,6 @@ void initPWM(){
 	DDRB |= (1 << DDB1); // PB1 as output (OC1A)
 	TCCR1A = 0;
 	TCCR1A |= (1 << COM1A1); // Set as non inverted
-	//TCCR1A |= (1 << WGM11) | (1 << WGM10); // Set mode 7 => Fast PWM and top = 0x03FF
 	TCCR1A |= (1 << WGM10); // Set mode 5 => Fast PWM and top = 0xFF
 	
 	
@@ -22,13 +21,13 @@ void initPWM(){
 
 void initADC(){
 	ADMUX = 0;
-	ADMUX	|= (1<<REFS0);  // Se ponen los 5V como referencia
+	ADMUX	|= (1<<REFS0);  // 5V as reference
 
-	ADMUX	|= (1 << ADLAR); // Justificaci[on a la izquierda
+	ADMUX	|= (1 << ADLAR); // Left justification
 	ADMUX	|= (1 << MUX1) | (1<< MUX0); //Select ADC3
 	
 	ADCSRA	= 0;
-	ADCSRA	|= (1 << ADPS1) | (1 << ADPS0); // Frecuencia de muestreo de 125kHz
+	ADCSRA	|= (1 << ADPS1) | (1 << ADPS0); // Sampling frequency = 125kHz "sampling = muestreo"
 	ADCSRA	|= (1 << ADIE); // Enable interruption
 	ADCSRA	|= (1 << ADEN); // Enable ADC
 	
